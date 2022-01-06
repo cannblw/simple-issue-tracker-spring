3cb8f38ed40d514d893fd962951039044fb2768d
Edgar Chirivella <edgar.95.12@gmail.com>
2022-01-06 22:14:39 +0100

    Create basic DeveloperController

---
+++ src/main/java/com/edgarchirivella/simpleissuetracker/controllers/DeveloperController.java
@@ -0,0 +1,38 @@
+package com.edgarchirivella.simpleissuetracker.controllers;
+
+import com.edgarchirivella.simpleissuetracker.domain.Developer;
+import com.edgarchirivella.simpleissuetracker.mappers.DeveloperMapper;
+import com.edgarchirivella.simpleissuetracker.services.DeveloperService;
+import dto.actions.CreateDeveloperAction;
+import dto.details.DeveloperDetails;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("developers")
+public class DeveloperController {
+    private final DeveloperService _developerService;
+    private final DeveloperMapper _developerMapper;
+
+    public DeveloperController(
+            DeveloperService developerService,
+            DeveloperMapper developerMapper) {
+        _developerService = developerService;
+        _developerMapper = developerMapper;
+    }
+
+    @GetMapping
+    public List<DeveloperDetails> GetAll() {
+        List<Developer> developers = _developerService.GetAll();
+
+        return _developerMapper.toDto(developers);
+    }
+
+    @PostMapping
+    public DeveloperDetails CreateDeveloper(@RequestBody CreateDeveloperAction action) {
+        Developer developer = _developerService.CreateDeveloper(action.name);
+
+        return _developerMapper.toDto(developer);
+    }
+}
