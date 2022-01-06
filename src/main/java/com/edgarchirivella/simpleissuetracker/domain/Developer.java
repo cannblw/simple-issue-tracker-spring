55b4dd82d263c42e99ff24c0b444af177eb5330c
Edgar Chirivella <edgar.95.12@gmail.com>
2022-01-06 20:56:53 +0100

    Create Developer service with methods to list and create developers

--- src/main/java/com/edgarchirivella/simpleissuetracker/domain/Developer.java
+++ src/main/java/com/edgarchirivella/simpleissuetracker/domain/Developer.java
@@ -1,11 +1,14 @@
 package com.edgarchirivella.simpleissuetracker.domain;
 
+import lombok.AllArgsConstructor;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "developers")
+@AllArgsConstructor
 public class Developer extends BaseEntity {
     @Column(name = "name")
     private String name;
---
+++ src/main/java/com/edgarchirivella/simpleissuetracker/services/DeveloperService.java
@@ -0,0 +1,27 @@
+package com.edgarchirivella.simpleissuetracker.services;
+
+import com.edgarchirivella.simpleissuetracker.domain.Developer;
+import com.edgarchirivella.simpleissuetracker.repositories.DeveloperRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DeveloperService {
+    private DeveloperRepository _developerRepository;
+
+    public DeveloperService(DeveloperRepository developerRepository) {
+        _developerRepository = developerRepository;
+    }
+
+    public List<Developer> GetAll() {
+        return _developerRepository.findAll();
+    }
+
+    public Developer CreateDeveloper(String name) {
+        Developer developer = new Developer(name);
+        _developerRepository.saveAndFlush(developer);
+
+        return developer;
+    }
+}
