b1487e8bb9185895967265ad6685973833d3b4d6
Edgar Chirivella <edgar.95.12@gmail.com>
2022-01-06 22:02:44 +0100

    Add Getters/Setters/Constructors to entities

--- src/main/java/com/edgarchirivella/simpleissuetracker/domain/Developer.java
+++ src/main/java/com/edgarchirivella/simpleissuetracker/domain/Developer.java
@@ -1,14 +1,20 @@
 package com.edgarchirivella.simpleissuetracker.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "developers")
-@AllArgsConstructor
 public class Developer extends BaseEntity {
     @Column(name = "name")
     private String name;
