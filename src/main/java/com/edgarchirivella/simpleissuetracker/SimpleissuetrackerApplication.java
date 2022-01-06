595ac357b329cf2de05799ca282f9d755686e757
Edgar Chirivella <edgar.95.12@gmail.com>
2022-01-06 19:56:13 +0100

    First commit

---
+++ src/main/java/com/edgarchirivella/simpleissuetracker/SimpleissuetrackerApplication.java
@@ -0,0 +1,13 @@
+package com.edgarchirivella.simpleissuetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleissuetrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleissuetrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edgarchirivella/simpleissuetracker/SimpleissuetrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edgarchirivella.simpleissuetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleissuetrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
