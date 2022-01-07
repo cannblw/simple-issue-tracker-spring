027e653087e91e66f0dadce75d2dbf5ac9673eb5
Edgar Chirivella <edgar.95.12@gmail.com>
2022-01-07 23:41:08 +0100

    Hide null fields of TicketDetails

--- src/main/java/com/edgarchirivella/simpleissuetracker/dto/details/TicketDetails.java
+++ src/main/java/com/edgarchirivella/simpleissuetracker/dto/details/TicketDetails.java
@@ -1,5 +1,6 @@
 package com.edgarchirivella.simpleissuetracker.dto.details;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,6 +8,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class TicketDetails {
     private Long id;
 
