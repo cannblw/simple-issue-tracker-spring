081e0a7bc81be8df91ab81d6a29ab1d796443d25
Edgar Chirivella <edgar.95.12@gmail.com>
2022-01-07 10:58:36 +0100

    Create interface out of Developer Service

--- src/main/java/com/edgarchirivella/simpleissuetracker/services/DeveloperService.java
+++ src/main/java/com/edgarchirivella/simpleissuetracker/services/DeveloperService.java
@@ -1,32 +1,12 @@
 package com.edgarchirivella.simpleissuetracker.services;
 
 import com.edgarchirivella.simpleissuetracker.domain.Developer;
-import com.edgarchirivella.simpleissuetracker.repositories.DeveloperRepository;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
-@Service
-public class DeveloperService {
-    private final DeveloperRepository _developerRepository;
-
-    public DeveloperService(DeveloperRepository developerRepository) {
-        _developerRepository = developerRepository;
-    }
-
-    public List<Developer> FindAll() {
-        return _developerRepository.findAll();
-    }
-
-    public Optional<Developer> FindById(Long id) {
-        return _developerRepository.findById(id);
-    }
-
-    public Developer CreateDeveloper(String name) {
-        var developer = new Developer(name);
-        _developerRepository.saveAndFlush(developer);
-
-        return developer;
-    }
+public interface DeveloperService {
+    List<Developer> FindAll();
+    Optional<Developer> FindById(Long id);
+    Developer CreateDeveloper(String name);
 }
---
+++ src/main/java/com/edgarchirivella/simpleissuetracker/services/DeveloperServiceImpl.java
@@ -0,0 +1,32 @@
+package com.edgarchirivella.simpleissuetracker.services;
+
+import com.edgarchirivella.simpleissuetracker.domain.Developer;
+import com.edgarchirivella.simpleissuetracker.repositories.DeveloperRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class DeveloperServiceImpl implements DeveloperService {
+    private final DeveloperRepository _developerRepository;
+
+    public DeveloperServiceImpl(DeveloperRepository developerRepository) {
+        _developerRepository = developerRepository;
+    }
+
+    public List<Developer> FindAll() {
+        return _developerRepository.findAll();
+    }
+
+    public Optional<Developer> FindById(Long id) {
+        return _developerRepository.findById(id);
+    }
+
+    public Developer CreateDeveloper(String name) {
+        var developer = new Developer(name);
+        _developerRepository.saveAndFlush(developer);
+
+        return developer;
+    }
+}
