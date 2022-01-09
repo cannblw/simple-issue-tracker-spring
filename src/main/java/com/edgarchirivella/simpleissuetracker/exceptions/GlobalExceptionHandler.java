09f8cafaa56eac7c191d49ed7b629a6e6a64c98d
Edgar Chirivella <edgar.95.12@gmail.com>
2022-01-09 21:12:25 +0100

    Override error handler for validation errors

--- src/main/java/com/edgarchirivella/simpleissuetracker/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/edgarchirivella/simpleissuetracker/exceptions/GlobalExceptionHandler.java
@@ -3,13 +3,19 @@ package com.edgarchirivella.simpleissuetracker.exceptions;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.HashMap;
 
 import static org.springframework.http.HttpStatus.*;
 
@@ -37,6 +43,23 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
             case NOT_FOUND -> CreateResponseError(ErrorMessages.RESOURCE_NOT_FOUND, NOT_FOUND);
             default -> throw ex;
         };
+    }
+
+    // See https://www.javaguides.net/2021/04/spring-boot-dto-validation-example.html
+    // Used to send validation error messages to the client
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers, HttpStatus status,
+                                                                  WebRequest request) {
+        var errors = new HashMap<String, String>();
+
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String message = error.getDefaultMessage();
+            errors.put(fieldName, message);
+        });
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 
     private ResponseEntity<ApplicationError> CreateResponseError(String message, HttpStatus status) {
